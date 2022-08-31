e771b39e70d4ebf177df139405cd648c15336c9c
Clara Moreira <anacacamm@gmail.com>
2022-08-31 10:50:28 -0300

    Header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
+import Head from 'next/head'
+
 export default function Home() {
   return (
-    <h1>Hello, dev!</h1>
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+    </>
   )
 }
