d5d6748a8af6e5156249ccdad0c9c823c160389d
Clara Moreira <anacacamm@gmail.com>
2022-09-21 17:21:50 -0300

    save user in database

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna'
 
 export const authOptions = {
   providers: [
@@ -9,5 +13,27 @@ export const authOptions = {
       scope: 'read:user'
     }),
   ],
+  jwt: {
+    signinKey: process.env.SIGNIN_KEY,
+  },
+  callbacks: {
+    async signIn({ user, account, profile, credentials }) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email }}
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+
+      return true
+    },
+  }
 }
 export default NextAuth(authOptions)
